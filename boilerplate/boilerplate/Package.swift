0a5215fcdc293116ea46b8e47867d85132064494
risk <azhanrizhan@gmail.com>
2025-05-09 01:04:13 +0800

    refactor: restructure project by removing old files and adding new boilerplate components

---
+++ boilerplate/boilerplate/Package.swift
@@ -0,0 +1,29 @@
+//// swift-tools-version:5.9
+//import PackageDescription
+//
+//let package = Package(
+//    name: "SwiftUI-MVVM-Boilerplate",
+//    platforms: [
+//        .iOS(.v16)
+//    ],
+//    products: [
+//        .library(
+//            name: "SwiftUI-MVVM-Boilerplate",
+//            targets: ["SwiftUI-MVVM-Boilerplate"]
+//        )
+//    ],
+//    dependencies: [
+//        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.25.0"),
+//        .package(url: "https://github.com/supabase-community/supabase-swift.git", from: "0.3.0")
+//    ],
+//    targets: [
+//        .target(
+//            name: "SwiftUI-MVVM-Boilerplate",
+//            dependencies: [
+//                .product(name: "RevenueCat", package: "purchases-ios"),
+//                .product(name: "Supabase", package: "supabase-swift")
+//            ],
+//            path: "SwiftUI-MVVM-Boilerplate"
+//        )
+//    ]
+//) 
